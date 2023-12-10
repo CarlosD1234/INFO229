53f9c396875616f6893a1049b3c2f91f01e4eba6
Acement <pj.cisterna@gmail.com>
2023-12-10 17:21:43 -0300

    Separar main.py de buscaminas.py

--- buscaminas.py
+++ buscaminas.py
@@ -83,29 +83,3 @@ class BuscaminasGUI:
             for c in range(self.ancho):
                 if self.tablero[r][c] == 'X':
                     self.buttons[r][c].config(text='X', bg='red')
-
-#Configura las dimensiones del tablero y la cantidad de minas mediante la entrada del usuario
-def config():
-    alto = int(input("Ingrese la altura del tablero: "))
-    ancho= int(input("Ingrese el ancho del tablero: "))
-    minas = int(input("Ingrese la cantidad de minas: "))
-
-    while minas >= (alto * ancho):
-        print("ERROR: la cantidad de minas no puede ser mayor o igual a la contidad de celdas\n")
-        minas = int(input("Ingrese la cantidad de minas: "))
-    
-    return alto, ancho, minas
-
-def main():
-
-    # Establece las dimensiones del juego
-    alto, ancho, minas = config()
-
-    root = tk.Tk()
-    root.title("Buscaminas")
-
-    BuscaminasGUI(root, alto, ancho, minas)
-    root.mainloop()
-
-if __name__ == "__main__":
-    main()
